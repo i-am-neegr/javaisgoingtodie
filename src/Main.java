c969cf3208225e0acb92aeb0fd0f75c0bcc2f198
i-am-neegr <166641557+i-am-neegr@users.noreply.github.com>
2024-10-31 01:09:45 +0300

    hw9

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,68 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("\ntask1\n");
+
+        int[] expenses = new int[5];
+
+        expenses[0] = 10;
+        expenses[1] = 20;
+        expenses[2] = 30;
+        expenses[3] = 40;
+        expenses[4] = 50;
+
+        int counterOfExpenses = 0;
+
+        for (int i = 0; i < expenses.length; i++) {
+            counterOfExpenses += expenses[i];
+        }
+
+        System.out.println("сумма трат за месяц - " + counterOfExpenses);
+
+        System.out.println("\ntask2\n");
+
+        int minimalExpenses = expenses[0];
+        int maximalExpenses = expenses[0];
+
+        for (int expence : expenses) {
+            if (expence < minimalExpenses) {
+                minimalExpenses = expence;
+            }
+            if (expence > maximalExpenses) {
+                maximalExpenses = expence;
+            }
+        }
+
+        System.out.println("минимальная трата - " + minimalExpenses);
+        System.out.println("максимальная трата - " + maximalExpenses);
+
+        System.out.println("\ntask3\n");
+
+        int totalExpenses = 0;
+        float averageExpenses;
+
+        for (int expence : expenses) {
+            totalExpenses += expence;
+        }
+
+        averageExpenses = (float) (totalExpenses / expenses.length);
+        System.out.println(averageExpenses);
+
+        System.out.println("\ntask4\n");
+
+        char[] reverseFullName = {'n', 'a', 'v', 'I', ' ', 'v', 'o', 'n', 'a', 'v', 'I'};
+        int length = reverseFullName.length;
+
+
+        for (int i = 0; i < length / 2; i++) {
+            final char left = reverseFullName[i];
+            final char right = reverseFullName[length - 1 - i];
+            reverseFullName[i] = right;
+            reverseFullName[length - 1 - i] = left;
+        }
+
+        System.out.println(reverseFullName);
+
+
     }
 }
